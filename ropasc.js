f1d7d081a7d1a2bf328787bbb4436a532527e8df
Jonas Elder <jonas.downer@gmail.com>
2020-02-12 18:54:01 -0800

    added gameplay function

--- ropasc.js
+++ ropasc.js
@@ -1,5 +1,24 @@
 function computerPlay (){
-    let computerChoose = ["Rock", "Paper", "Scissors"];
-    let computerGo = computerChoose[Math.floor((Math.random() * 3))];
-    return computerGo
+    let computerChoose = ["rock", "paper", "scissors"];
+    let computerPlay = computerChoose[Math.floor((Math.random() * 3))];
+    return computerPlay
+}
+
+function gamePlay (){
+    let playerGo = prompt("Rock, Paper, or Scissors?");
+    let computerGo = computerPlay();
+if (playerGo.toLowerCase() == computerGo){
+    return "Tie Game";
+}
+else if (playerGo.toLowerCase() == "rock" && computerGo == "scissors" ||
+         playerGo.toLowerCase() == "paper" && computerGo == "rock" ||
+         playerGo.toLowerCase() == "scissors" && computerGo == "paper"){
+    return "You Win!";
+}
+else if (playerGo.toLowerCase() != "rock" || "paper" || "scissors"){
+    return "Incorrect input. Please type Rock, Paper, or Scissors"
+}
+else {
+    return "You Lost.";
+}
 }
