ad5341bbce904f9e4a78cf4be991cf597e5f9a4a
Jonas Elder <jonas.downer@gmail.com>
2020-02-15 12:38:45 -0800

    added player's selection function for game. DRY is an issue, but it works

--- ropasc.js
+++ ropasc.js
@@ -1,24 +1,32 @@
-function computerPlay (){
+function computerGo(){
     let computerChoose = ["rock", "paper", "scissors"];
     let computerPlay = computerChoose[Math.floor((Math.random() * 3))];
     return computerPlay
 }
 
-function gamePlay (){
-    let playerGo = prompt("Rock, Paper, or Scissors?");
-    let computerGo = computerPlay();
-if (playerGo.toLowerCase() == computerGo){
-    return "Tie Game";
-}
-else if (playerGo.toLowerCase() == "rock" && computerGo == "scissors" ||
-         playerGo.toLowerCase() == "paper" && computerGo == "rock" ||
-         playerGo.toLowerCase() == "scissors" && computerGo == "paper"){
-    return "You Win!";
-}
-else if (playerGo.toLowerCase() != "rock" || "paper" || "scissors"){
-    return "Incorrect input. Please type Rock, Paper, or Scissors"
-}
-else {
-    return "You Lost.";
-}
+function playerGo(){
+    let playerChoose = prompt("Enter Rock, Paper, or Scissors to play.");
+    while (playerChoose === null){
+        alert("Please make a valid entry. Rock, paper, or scissors.");
+        playerChoose = prompt("Enter Rock, paper, or scissors.");
+    }
+    playerChoose = playerChoose.toLowerCase();
+    if (playerChoose == "rock" || playerChoose == "paper" || playerChoose == "scissors"){
+        alert("good choice")
+        playerPlay = playerChoose;
+        return playerPlay;
+    }
+    else{
+        do{
+            playerChoose = prompt("Enter a valid selection. Rock, paper, or scissors");
+            while (playerChoose === null){
+                alert("Please make a valid selection.");
+                playerChoose = prompt("Enter rock, paper, or scissors");
+            }
+            playerChoose.toLowerCase();
+        }
+        while (playerChoose != "rock" && playerChoose != "paper" && playerChoose != "scissors")
+    }
+    playerPlay = playerChoose;
+    return playerPlay;
 }
